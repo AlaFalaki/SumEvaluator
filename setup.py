b84a831c94f12aaf2bc2005606443f439908fa48
AlaFalaki <ala.falaki@gmail.com>
2024-01-05 14:09:04 -0800

    Fix dependencies...

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='SumEvaluator',
     packages=find_packages(),
     version='0.1.0',
-    description='A number of complimentary metrics to assess a summarization model.',
+    description='A library designed for the analysis of various aspects of generated summaries.',
     author='Ala Falaki',
     license='MIT',
-    install_requires=[]
+    install_requires=["torch", "nltk", "sentence_transformers", "pandas", "openai", "six"]
 )
